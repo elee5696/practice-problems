c6f6a8e58ab63ebf7aae1d38e8031f000063a323
Edward W. Lee <elee5696@gmail.com>
2019-08-14 03:00:45 -0700

    Drill01 - Edward Lee

--- drills01/yourcode.js
+++ drills01/yourcode.js
@@ -1,16 +1,40 @@
 
-function sumArray(){
-	
+function sumArray(numbers){
+  var number;
+  var sum = null;
+  for(number of numbers) {
+    sum += number;
+  }
+  return sum;
 }
 
-function fitWithinVal(){
-
+function fitWithinVal(numbers, target){
+  var number;
+  var sum = 0;
+  var newArr = [];
+  for(number of numbers) {
+    if(number + sum >= target) {
+      continue;
+    } else {
+      sum += number;
+      newArr.push(number);
+    }
+  }
+  return newArr;
 }
 
-function getAllNamesShorterThan(){
-
+function getAllNamesShorterThan(names, target){
+  var name;
+  var newArr = [];
+  for(name of names) {
+    if(name.length < target) {
+      newArr.push(name);
+    }
+  }
+  return newArr;
 }
 
-function makeLabel(){
-
+function makeLabel(person){
+  var newLabel = person.greeting + " " + person.givenName + " " + person.familyName + "\n" + person["home address"].streetNumber + " " + person["home address"].streetName + "\n" + person["home address"].city + ", " + person["home address"].state + " " + person["home address"].zip;
+  return newLabel;
 }
